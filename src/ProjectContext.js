b3c76a583c2057f60862ba168c9aac6b76684394
GUchechi <godswilluchendu7@gmail.com>
2022-08-25 20:28:17 +0100

    added useState

--- src/ProjectContext.js
+++ src/ProjectContext.js
@@ -1,10 +1,17 @@
-import React, { Children, createContext, useState} from 'react';
+import React, { createContext, useState} from 'react';
 
 export const ProjectContext = createContext();
 
 export const ProjectState = ({children}) => {
+
+    const [name, setName] = useState("")
+
+    const handler = () => {
+        console.log("Hello world")
+    }
+
     return (
-        <ProjectContext.Provider>
+        <ProjectContext.Provider value={{name, setName, handler}}>
             {children}
         </ProjectContext.Provider>
     )
