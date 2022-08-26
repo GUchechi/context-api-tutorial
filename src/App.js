7bef6f0e8e63be821cf7cf337d7246d6ce952095
GUchechi <godswilluchendu7@gmail.com>
2022-08-26 20:02:12 +0100

    arranged my app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React,  {useContext} from 'react';
+import React from 'react';
 import './App.css';
 import Contact from './components/Contact';
-import { ProjectState } from './ProjectContext';
 
 function App() {
   return (
-    <ProjectState>
+
       <div className="App">
         <Contact />
       </div>
-    </ProjectState>
-  );
+  )
 }
 
 export default App;
