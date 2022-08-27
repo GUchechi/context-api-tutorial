2a1f947c4e623174adbe64e45de807067d8a0f71
GUchechi <godswilluchendu7@gmail.com>
2022-08-27 21:06:21 +0100

    finished contact

--- src/ProjectContext.js
+++ src/ProjectContext.js
@@ -1,9 +1,16 @@
-import React, {createContext, useState} from 'react';
+import React, {createContext, useState, useContext} from 'react';
 
 export const ProjectContext = createContext();
+
 export const ProjectState = ({children}) => {
+    const [name, setName] = useState("Millionaire");
+
+    const handler = () => {
+        setName("I'm rich in Christ Jesus");
+    }
+
     return (
-        <ProjectContext.Provider>
+        <ProjectContext.Provider value={{name, setName, handler}}>
             {children}
         </ProjectContext.Provider>
     )
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,14 @@
 import React,{useContext} from 'react'
 import ContactInfo from '../ContactInfo'
-
+import {ProjectContext} from '../ProjectContext'
 
 const Contact = () => {
+  const {name, handler} = useContext(ProjectContext)
 
   return (
     <div>
-        <h2>1</h2>
-        <h2>2</h2>
+        <h2>{name}</h2>
+        <h2 onClick={handler}>2</h2>
         <h2>3</h2>
         <ContactInfo />
     </div>
