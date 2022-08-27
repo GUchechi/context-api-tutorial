c41fda84a727c67a983a3d470cf46697974bd8ca
GUchechi <godswilluchendu7@gmail.com>
2022-08-27 21:08:58 +0100

    finished contactInfo

--- src/ContactInfo.jsx
+++ src/ContactInfo.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ProjectContext } from './ProjectContext'
 
 const ContactInfo = () => {
+  const {name, handler, number} = useContext(ProjectContext)
     
   return (
     <div>
-        <h1>Info</h1>
+        <h1>{number}</h1>
     </div>
   )
 }
--- src/ProjectContext.js
+++ src/ProjectContext.js
@@ -4,13 +4,14 @@ export const ProjectContext = createContext();
 
 export const ProjectState = ({children}) => {
     const [name, setName] = useState("Millionaire");
+    const [number, setNumber] = useState("10000000000");
 
     const handler = () => {
         setName("I'm rich in Christ Jesus");
     }
 
     return (
-        <ProjectContext.Provider value={{name, setName, handler}}>
+        <ProjectContext.Provider value={{name, setName, handler, number}}>
             {children}
         </ProjectContext.Provider>
     )
